399aff6acc1c347b44382858cce278759f4e5c83
RomanBeyko <r.beyko@rauschert.pro>
2024-01-16 12:36:52 +0200

    chore: init project
    [Task]: #
    feat: create new project
    
    [Notes]
    <notes>

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h3>Header</h3>
+      </header>
+
+      <main>
+        <h3>Body</h3>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+
+        <ol>
+          <li>*</li>
+          <li>**</li>
+          <li>***</li>
+        </ol>
+
+        <input type="text" />
+      </main>
+
+      <footer>
+        <h3>Footer</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
